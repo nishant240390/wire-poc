2d85b251a03b82527174bb40cbf7f04ea3c18002
nishantagrawal009 <nishant.agrawal@razorpay.com>
2020-05-04 10:26:09 +0530

    removed. unused items for now

--- Got/MockProviders.go
+++ Got/MockProviders.go
@@ -6,17 +6,3 @@ func ProvideMockBeyondWall()*House.MockBeyondWall {
 	n := new(House.MockBeyondWall)
 	return n
 }
-
-func ProvideMockStarks() *House.MockStark {
-	n := new(House.MockStark)
-	return n
-}
-
-func ProvideMockLannisters() *House.MockLannisters{
-	n := new(House.MockLannisters)
-	return n
-}
-
-func NewMockWar(h1 House.Stark , h2 House.Lannisters, h3 House.IHouse) MockWar{
-	return MockWar{house1:h1,house2:h2, house3:h3}
-}
