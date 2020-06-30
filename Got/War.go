ec56c5bf596be80f0b6a866d05d9f361e8c168d7
nishant240390 <nishantagrawal009@gmail.com>
2020-06-30 12:30:06 +0530

    Adding docker (#2)
    * updated the package structure
    
    * demo of cicular dependency
    
    Co-authored-by: nishantagrawal009 <nishant.agrawal@razorpay.com>

--- Got/War.go
+++ Got/War.go
@@ -8,7 +8,8 @@ import (
 type War struct{
 	House1 House.Stark
 	House2 House.Lannisters
-	House3 House.IHouse
+	House3 House.BeyondWall
+	WarZone *WarZone
 }
 func (w *War) startWar() {
 	fmt.Printf("%s\n%s\n%s",w.House1.Fight(),w.House2.Fight(),w.House3.Fight())
--- Got/Wire_test.go
+++ Got/Wire_test.go
@@ -2,9 +2,47 @@ package got
 
 import (
 	"testing"
+	"wire-poc/House"
 )
-
-func TestInitialise(t *testing.T) {
+//
+//func TestInitialiseUsingWire(t *testing.T) {
+//	tests := []struct {
+//		name    string
+//	}{
+//		{
+//			name: "name",
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			got, err := Initialize()
+//			if  err != nil{
+//				t.Errorf("Initialise() error ")
+//				return
+//			}
+//			got.startWar()
+//		})
+//	}
+//}
+//func TestInitialiseWithoutUsingWireForError(t *testing.T) {
+//	tests := []struct {
+//		name    string
+//	}{
+//		{
+//			name: "name",
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			stark := House.Stark{Leader:"john"}
+//			lannister := House.Lannisters{"cersie"}
+//			beyond := House.BeyondWall{"NightKing"}
+//			war := War{House1:stark,House2:lannister,House3:beyond }
+//			war.startWar()
+//		})
+//	}
+//}
+func TestInitialiseWithoutUsingWireForCircular(t *testing.T) {
 	tests := []struct {
 		name    string
 	}{
@@ -14,33 +52,12 @@ func TestInitialise(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := Initialize()
-			if  err != nil{
-				t.Errorf("Initialise() error ")
-				return
-			}
-			got.startWar()
-		})
-	}
-}
-
-
-func TestInitialiseMock(t *testing.T) {
-	tests := []struct {
-		name    string
-	}{
-		{
-			name: "name",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := InitializeMock()
-			if  err != nil{
-				t.Errorf("Initialise() error ")
-				return
-			}
-			got.startWar()
+			stark := House.Stark{Leader:"john"}
+			lannister := House.Lannisters{"cersie"}
+			beyond := House.BeyondWall{"NightKing"}
+			zone :=  WarZone{name:"casterock", War:nil}
+			war := War{House1:stark,House2:lannister,House3:beyond,WarZone:&zone }
+			war.startWar()
 		})
 	}
 }
